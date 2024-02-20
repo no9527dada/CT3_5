08c7054e4af041e59cbf551fde97dd8b0f3056d0
no9527dada <cyj674971336@163.com>
2024-02-20 19:06:15 +0800

    Initial commit

---
+++ src/ct_5/content/单位5.java
@@ -0,0 +1,15 @@
+package ct_5.content;
+
+import mindustry.entities.Units;
+
+//方块
+public class 单位5 {
+    public static Units
+
+            单位;
+
+    public static void load() {
+
+
+    }
+}
---
+++ src/ct_5/ct3_5.java
@@ -0,0 +1,12 @@
+package ct_5;
+
+
+import ct_5.content.chapter5;
+import mindustry.mod.Mod;
+
+public class ct3_5  extends Mod {
+    public void loadContent() {
+        chapter5.load();
+
+    }
+}
